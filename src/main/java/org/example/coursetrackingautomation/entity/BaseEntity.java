663ab63cfd752eda6336bd207451a48139ab2f5c
yunussgultekinn <yunussgultekiin@gmail.com>
2025-12-20 22:20:22 +0300

    BaseEntity

--- src/main/java/org/example/coursetrackingautomation/entity/BaseEntity.java
+++ src/main/java/org/example/coursetrackingautomation/entity/BaseEntity.java
@@ -1,11 +1,19 @@
 package org.example.coursetrackingautomation.entity;
-import jakarta.persistence.Column;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Data;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-import java.time.LocalDateTime;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
 
 @MappedSuperclass
-@Data
-public abstract class BaseEntity {}
+@Getter
+@Setter
+@EqualsAndHashCode
+public abstract class BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+}
